d5cd4c05d6f16876480eab606f1a55fdd2a07286
AgentDaun <agent47daun@gmailcom>
2021-08-02 17:28:16 +0400

    Converting items stats into data like POETRADE_ITEM_ID:ROLL_RANGE

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import requests
 import xlsxwriter
 import os
 import openpyxl
+from json.decoder import JSONDecodeError
 
 ALL_POSSIBLE_CATEGORIES = ['accessory', 'armour', 'weapon', 'jewel', 'uniqueMap']
+POETRADE_HEADERS = {'User-Agent': 'agent47daun@gmail.com'}
 
 
 class PoeFlipper:
@@ -12,37 +14,86 @@ class PoeFlipper:
     def __init__(self, league, filename="item_table.xlsx"):
         self.league = league
         self.filename = filename
-        self.poewatch_get_url = "https://api.poe.watch/get?category={}&league={league}".format("{}", league=league)
-        self.poetrade_search = "https://www.pathofexile.com/api/trade/search/{}".format(league)
-        self.poetrade_fetch = "https://www.pathofexile.com/api/trade/fetch/{}?query={}"
+
         self.parsed_items_data = []
         self.result_items_data = []
 
-    def parse_items_data(self):
+        self.poewatch_get_url = "https://api.poe.watch/get?category={}&league={league}".format("{}", league=league)
+        self.poetrade_search = "https://www.pathofexile.com/api/trade/search/{}".format(league)
+        self.poetrade_fetch = "https://www.pathofexile.com/api/trade/fetch/{}?query={}"
+        self.poetrade_stats = "https://www.pathofexile.com/api/trade/data/stats"
+
+    def parse_items_data(self) -> None:
+        try:
+            stats_request = requests.get(self.poetrade_stats, headers=POETRADE_HEADERS)
+            stats_data = stats_request.json()
+        except JSONDecodeError:
+            print("Stats fetch failed. Try again in 10 sec..")
+            raise SystemExit
+
+        converted_stats_explicit = {}
+        stats = stats_data['result']
+        for i in stats:
+            if i['label'] == "Explicit":
+                for b in i['entries']:
+                    converted_stats_explicit[b['text']] = b['id']
+
         for item in self.parsed_items_data:
             links_price = False  # Price depends on links(6-links)
-            explicits_range = []  # Price depends on explicit rolls
-            implicits_range = []  # Price depends on implicit rolls if item corrupted
+            explicits_converted = {}  # Price depends on explicit rolls
+            implicits_converted = {}  # Price depends on implicit rolls if item corrupted
 
             if item.get('explicits'):
                 explicits = item['explicits'].split("&")
                 for explicit in explicits:
                     try:
-                        explicit = explicit.split("(")[1].split(")")[0]
+                        if explicit.startswith("(") or explicit.startswith("+("):
+                            explicit_raw = explicit.split("(")[1].split(")")
+                            explicit_range = explicit_raw[0]
+                            explicit_text = "#" + explicit_raw[1]
+                            if explicit.startswith("+"):
+                                explicit_text = "+" + explicit_text
+                            explicit_range = explicit_range.split("-")
+                        elif explicit.startswith("Socketed Gems are"):
+                            explicit_range = [explicit[36:39].strip(), ]
+                            explicit_text = explicit[:36] + " # " + explicit[39:]
+                            explicit_text = explicit_text.replace("  ", " ")
+                        else:
+                            # How it works:
+                            # explicit = Adds (65-75) to (100-110) Physical Damage" for example
+                            # explicit.split("(") => ['Adds ', '65-75) to ', '100-110) Physical Damage']
+                            # explicit.split("(")[1].split(")") => ('65-75', ' to ')
+                            # explicit.split("(")[2].split(")") => ('100-110', ' Physical Damage')
+                            # explicit.split("(")[0] + "#" + to + "#" + bonus => Adds # to # Physical Damage
+                            explicit_range_first, to = explicit.split("(")[1].split(")")
+                            explicit_range_second, bonus = explicit.split("(")[2].split(")")
+                            explicit_range = [explicit_range_first.split("-"), explicit_range_second.split("-")]
+                            explicit_text = explicit.split("(")[0] + "#" + to + "#" + bonus
                     except IndexError:
                         continue
-                    explicit_range = explicit.split("-")
-                    explicits_range.append(explicit_range)
 
+                    try:
+                        if converted_stats_explicit.get(explicit_text):
+                            explicit_id = converted_stats_explicit[explicit_text]
+                        elif converted_stats_explicit.get(explicit_text + " (Local)"):
+                            explicit_id = converted_stats_explicit[explicit_text + " (Local)"]
+                        else:
+                            explicit_id = converted_stats_explicit[explicit_text[1:]]
+                    except KeyError:
+                        continue
+                    explicits_converted[explicit_id] = explicit_range
+
+            #  TODO: converted_stats_implicit
             if item.get('implicits'):
                 implicits = item['implicits'].split("&")
                 for implicit in implicits:
                     try:
-                        implicit = implicit.split("(")[1].split(")")[0]
+                        implicit = implicit.split("(")[1].split(")")
+                        implicit_range = implicit[0]
                     except IndexError:
                         continue
-                    implicit_range = implicit.split("-")
-                    implicits_range.append(implicit_range)
+                    implicit_range = implicit_range.split("-")
+                    implicits_converted["#" + implicit[1]] = implicit_range
 
             if item['group'] == "bodyarmours":
                 links_price = True
@@ -52,8 +103,8 @@ class PoeFlipper:
                 "group": item['group'],
                 "name": item['name'],
                 "mean": item['mean'],
-                "explicits_range": explicits_range,
-                "implicits_range": implicits_range,
+                "explicits": explicits_converted,
+                "implicits": implicits_converted,
                 "depends_on_links": links_price,
             })
 
@@ -72,6 +123,21 @@ class PoeFlipper:
             })
         self.parse_items_data()
 
+    # def check_price(self, item) -> None:
+    #     search_query = {
+    #         "query": {
+    #             "status": {
+    #                 "option": "online"
+    #             },
+    #             "name": item["name"],
+
+    #         }
+    #     }
+    #     try:
+    #         request = requests.post(self.poetrade_search, data=)
+    #     except JSONDecodeError as e:
+    #         pass
+
     def start(self) -> None:
         if os.path.isfile(self.filename):
             print("Found {} table. Processing..".format(self.filename))
@@ -81,7 +147,8 @@ class PoeFlipper:
             return self.start()
 
         self.parse_file()
-        print(self.result_items_data)
+        # for item in self.result_items_data:
+        #     zxc = self.check_price(item)
 
     def generate_items_table(self, categories_to_flip, custom_filename=None) -> None:
         workbook = xlsxwriter.Workbook("item_table.xlsx" if not custom_filename else "{}.xlsx".format(custom_filename))
@@ -92,8 +159,8 @@ class PoeFlipper:
             try:
                 request = requests.get(self.poewatch_get_url.format(category))
                 items_data = request.json()
-            except Exception as e:
-                print(e)
+            except JSONDecodeError as e:
+                print(e, request)
                 continue
 
             ws.write(0, 0, "Category")
